d5b52b748d309cd2b1fa0fbf4a3dd2db7378e846
Mark <mjc862@mail.usask.ca>
2018-11-06 06:33:39 -0600

    Added Skeleton with grid class in it

---
+++ A-Star Individual/Assets/Node.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Node : IComparer {
+
+    public Vector2 point;
+
+    public int gCost;
+    public int hCost;
+
+    public int FCost {
+        get {
+            return this.gCost + this.hCost;
+        }
+    }
+
+    public bool walkable = true;
+
+    /*
+     * Constructor method that builds our node. 
+     * :param gCost -> Distance from one node to the next node.
+     * :param hCost -> Heuristic Cost, which will be the distance from this node to goal node.
+     * :param x,y coordinate -> (x,y) location in which this node is placed in.
+     */ 
+    public Node(int xCoordinate, int yCoordinate, int gCost, int hCost) {
+        this.point = new Vector2(xCoordinate, yCoordinate);
+        this.gCost = gCost;
+        this.hCost = hCost;
+    }
+
+    /*
+     * Comparator for this node object, for ordering nodes in accordance to their fCost!
+     * 1 if x > y
+     * -1 if x < y
+     * 0 if x == y
+     */ 
+    public int Compare(object x, object y) {
+        Node node1 = (Node) x;
+        Node node2 = (Node) y;
+
+        if (node1.FCost > node2.FCost) {
+            return 1;
+        }
+
+        else if (node1.FCost < node2.FCost) {
+            return -1;
+        }
+        
+        else {
+            return 0;
+        }
+    }
+}
---
+++ A-Star Individual/Assets/WorldGrid.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGrid : MonoBehaviour {
+
+    public Node[,] gridNode;
+    int cellSize = 1;
+    [Range(1, 1000)] public int gridX = 1;
+    [Range(1, 1000)] public int gridY = 1;
+
+    private void Start() {
+        gridNode = new Node[gridX, gridY];
+        for (int i = 0; i < gridX; i++) {
+            for (int j = 0; j < gridY; j++) {
+                gridNode[i, j] = new Node(i, j, 0, 0);
+            }
+        }
+    }
+
+    private void OnDrawGizmos() {
+
+        Gizmos.DrawWireCube(transform.position, new Vector2(gridX, gridY));
+        Vector2 cell = new Vector2(cellSize, cellSize);
+        Vector2 bottomEdge = (Vector2) transform.position + (cell + Vector2.down * gridY) / 2;
+        Vector2 leftEdge = (Vector2.left * gridX) / 2;
+        Vector2 leftCorner = (bottomEdge + leftEdge);
+
+        for (int i = 0; i < gridX; i++) {
+            for (int j = 0; j < gridY; j++) {
+                Vector2 pos = new Vector2(i, j);
+                Gizmos.DrawWireCube(leftCorner + pos, cell);
+            }
+        }
+    }
+}
